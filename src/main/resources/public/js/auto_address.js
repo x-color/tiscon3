7732011e03b7f180eeabca5ba8522837cc1805cd
x-color <36035885+x-color@users.noreply.github.com>
2018-02-23 14:31:51 +0900

    Add automatic input employerAddress

--- src/main/resources/public/js/auto_address.js
+++ src/main/resources/public/js/auto_address.js
@@ -25,25 +25,28 @@
 //     }
 //   });
 // });
-function getAddress() {
-  var count = $(this).val().length;
-  if (count == 8) {
-    $.ajax({
-        url: 'http://zipcloud.ibsnet.co.jp/api/search' + '?zipcode=' + $('[name="zipCode"]').val(),
-        dataType: 'jsonp',
-        type: 'get'
-      })
-      .done((res) => {
-        if (res["status"] == "200") {
-          var results = res["results"][0];
-          var addr = results["address1"] + results["address2"] + results["address3"];
-          if ($('[name="address"]').val().indexOf(addr) == -1) {
-            $('[name="address"]').val(addr);
-          }
-        }
-      });
-  }
-}
+
 $(function() {
-  $('[name="zipCode"]').on('keydown keyup keypress change', getAddress());
+  function getAddress(args) {
+    var count = $(this).val().length;
+    var addrForm = $(args.data.addr);
+    if (count == 8) {
+      $.ajax({
+          url: 'http://zipcloud.ibsnet.co.jp/api/search' + '?zipcode=' + $(args.data.zipcode).val(),
+          dataType: 'jsonp',
+          type: 'get'
+        })
+        .done((res) => {
+          if (res["status"] == "200") {
+            var results = res["results"][0];
+            var addr = results["address1"] + results["address2"] + results["address3"];
+            if (addrForm.val().indexOf(addr) == -1) {
+              addrForm.val(addr);
+            }
+          }
+        });
+    }
+  }
+  $('[name="zipCode"]').on('keydown keyup keypress change', {addr:'[name="address"]', zipcode:'[name="zipCode"]'}, getAddress);
+  $('[name="employerZipCode"]').on('keydown keyup keypress change', {addr:'[name="employerAddress"]', zipcode:'[name="employerZipCode"]'}, getAddress);
 });
